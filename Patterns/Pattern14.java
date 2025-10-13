c905dd2000e8e6b2e7432741110e5346353f1eec
Ashraf <66486635+khan-Ashraf-source@users.noreply.github.com>
2025-10-13 18:09:05 +0530

    Pattern 14 completed

---
+++ Patterns/Pattern14.java
@@ -0,0 +1,22 @@
+/*
+//Pattern to print
+
+A
+AB
+ABC
+ABCD
+ABCDE
+
+*/
+
+
+public class Pattern14{
+    public static void main(String[] args){
+        for(int i=0; i<5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print((char)(65+j));
+            }
+            System.out.println();
+        }
+    }
+}
